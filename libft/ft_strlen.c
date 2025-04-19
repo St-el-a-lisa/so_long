865001b06dd179e3c76e1c0c9d838a248f4822b9
St-el-a-lisa <stelacid@gmail.com>
2025-04-19 19:14:48 +0200

    counter conllectible and found plyer

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,28 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecid <ecid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/23 17:31:06 by ecid              #+#    #+#             */
-/*   Updated: 2025/04/19 18:58:28 by ecid             ###   ########.fr       */
+/*   Created: 2024/11/13 19:07:53 by ecid              #+#    #+#             */
+/*   Updated: 2025/04/19 18:58:18 by ecid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include "ft_printf/ft_printf.h"
-# include <stddef.h>
-# include <stdint.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+int	ft_strlen(char *str)
+{
+	int	i;
 
-int		ft_atoi(const char *str);
-char	*ft_strdup(char *src);
-int		ft_strlen(char *str);
-
-#endif
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
--- srcs/is_valid.c
+++ srcs/is_valid.c
@@ -1,14 +1,95 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   valid.c                                            :+:      :+:    :+:   */
+/*   is_valid.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ecid <ecid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:40:56 by ecid              #+#    #+#             */
-/*   Updated: 2025/04/19 17:37:45 by ecid             ###   ########.fr       */
+/*   Updated: 2025/04/19 19:09:33 by ecid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_printf/ft_printf.h"
 #include "so_long.h"
+
+char	**copy_map(char **map, int height)
+{
+	char	**new_map;
+	int		i;
+
+	new_map = malloc(sizeof(char *) * (height + 1));
+	if (!new_map)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		new_map[i] = ft_strdup(map[i]);
+		if (!new_map[i])
+		{
+			while (--i >= 0)
+				free(new_map[i]);
+			free(new_map);
+			return (NULL);
+		}
+		i++;
+	}
+	new_map[i] = NULL;
+	return (new_map);
+}
+
+void	free_map(char **map)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
+}
+
+int	count_collectibles(char **map)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'C')
+				count++;
+			j++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+void	find_player(char **map, int *x, int *y)
+{
+	int i = 0;
+	int j;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'P')
+			{
+				*x = j;
+				*y = i;
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}

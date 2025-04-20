e94356f5837bbba7879d514504c78081b5c7e157
St-el-a-lisa <stelacid@gmail.com>
2025-04-20 18:38:21 +0200

    test map2 sujet

--- srcs/solong.c
+++ srcs/solong.c
@@ -6,7 +6,7 @@
 /*   By: ecid <ecid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:40:56 by ecid              #+#    #+#             */
-/*   Updated: 2025/04/20 17:58:16 by ecid             ###   ########.fr       */
+/*   Updated: 2025/04/20 18:37:15 by ecid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,21 @@ int	main(void)
 {
 	t_game	game;
 	int		height;
+	int		width;
+	int		tile_size;
 
+	tile_size = 32;
+	game.map = load_map("maps/map2_sujet.ber");
+	height = get_map_height(game.map);
+	width = get_map_width(game.map);
 	game.mlx = mlx_init();
-	game.win = mlx_new_window(game.mlx, 320, 320, "Window");
-	game.img.img = mlx_new_image(game.mlx, 320, 320);
+	game.win = mlx_new_window(game.mlx, width * tile_size, height * tile_size,
+			"Window");
+	game.img.img = mlx_new_image(game.mlx, width * tile_size, height
+			* tile_size);
 	game.img.addr = mlx_get_data_addr(game.img.img, &game.img.bits_per_pixel,
 			&game.img.line_length, &game.img.endian);
 	load_images(game.mlx, &game.imgs);
-	game.map = load_map("maps/map_OG.ber");
-	height = get_map_height(game.map);
 	validate_map(game.map, height);
 	find_player_position(&game);
 	print_map_graphics(game.mlx, game.win, &game.imgs, game.map);

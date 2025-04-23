4cce63744e53b17dfb01e3983424755e41c0ebb2
St-el-a-lisa <stelacid@gmail.com>
2025-04-23 19:52:56 +0200

    re norme

--- srcs/solong.c
+++ srcs/solong.c
@@ -6,34 +6,44 @@
 /*   By: ecid <ecid@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:40:56 by ecid              #+#    #+#             */
-/*   Updated: 2025/04/23 19:33:29 by ecid             ###   ########.fr       */
+/*   Updated: 2025/04/23 19:47:41 by ecid             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	init_game(t_game *game)
+{
+	ft_bzero(game, sizeof(t_game));
+	game->tile_size = 32;
+	game->map = load_map("maps/invalid_map3.ber", game);
+	if (!game->map)
+		error_and_exit("Échec du chargement de la map", game);
+	game->map_height = get_map_height(game->map);
+	game->map_width = get_map_width(game->map);
+	validate_map(game);
+}
+
+static void	init_window(t_game *game)
+{
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		error_and_exit("Échec de l'initialisation de mlx", game);
+	game->win = mlx_new_window(game->mlx, game->map_width * game->tile_size,
+			game->map_height * game->tile_size,
+			"~ ^_^ Marshmallow Barista ^_^ ~");
+	game->img.img = mlx_new_image(game->mlx, game->map_width * game->tile_size,
+			game->map_height * game->tile_size);
+	game->img.addr = mlx_get_data_addr(game->img.img, &game->img.bits_per_pixel,
+			&game->img.line_length, &game->img.endian);
+}
+
 int	main(void)
 {
 	t_game	game;
 
-	ft_bzero(&game, sizeof(t_game));
-	game.tile_size = 32;
-	game.map = load_map("maps/map1_sujet.ber", &game);
-	if (!game.map)
-		error_and_exit("Échec du chargement de la map", &game);
-	game.map_height = get_map_height(game.map);
-	game.map_width = get_map_width(game.map);
-	validate_map(&game);
-	game.mlx = mlx_init();
-	if (!game.mlx)
-		error_and_exit("Échec de l'initialisation de mlx", &game);
-	game.win = mlx_new_window(game.mlx, game.map_width * game.tile_size,
-			game.map_height * game.tile_size,
-			"~ ^_^ Marshmallow Barista ^_^ ~");
-	game.img.img = mlx_new_image(game.mlx, game.map_width * game.tile_size,
-			game.map_height * game.tile_size);
-	game.img.addr = mlx_get_data_addr(game.img.img, &game.img.bits_per_pixel,
-			&game.img.line_length, &game.img.endian);
+	init_game(&game);
+	init_window(&game);
 	load_images(game.mlx, &game.imgs, &game);
 	find_player_position(&game);
 	print_map_graphics(&game);
